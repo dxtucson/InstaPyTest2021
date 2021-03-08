38726b25b1cbf67397c484e9dbceeea90a8454e3
dxtucson <dixiao@email.arizona.edu>
2021-03-07 19:47:28 -1000

    Update main.py
    Added more places.
    Wait longer time between likes.

--- main.py
+++ main.py
@@ -25,19 +25,19 @@ if __name__ == '__main__':
             session.set_quota_supervisor(enabled=True,
                                          sleep_after=["likes", "comments_d", "follows", "unfollows", "server_calls_h"],
                                          sleepyhead=True, stochastic_flow=True, notify_me=True,
-                                         peak_likes_hourly=31,
-                                         peak_likes_daily=301,
+                                         peak_likes_hourly=30,
+                                         peak_likes_daily=480,
                                          peak_comments_hourly=21,
                                          peak_comments_daily=182,
                                          peak_follows_hourly=30,
                                          peak_follows_daily=500,
                                          peak_unfollows_hourly=35,
                                          peak_unfollows_daily=402,
-                                         peak_server_calls_hourly=200,
+                                         peak_server_calls_hourly=150,
                                          peak_server_calls_daily=3000)
-            session.set_skip_users(skip_private=True)
-            session.set_do_story(enabled=True, percentage=10, simulate=True)
-            session.set_user_interact(amount=10, randomize=True, percentage=10, media='Photo')
+            # session.set_skip_users(skip_private=True)
+            # session.set_do_story(enabled=True, percentage=5, simulate=True)
+            # session.set_user_interact(amount=10, randomize=True, percentage=10, media='Photo')
             # session.follow_user_followers([
             #                                'twoladieskitchen',
             #                                'hideawayhilo',
@@ -51,8 +51,27 @@ if __name__ == '__main__':
             #                                'tetsumenhawaii', 'marmaidz', 'kuiandiflorist', 'yourstrulymeri',
             #                                'kiaannii'],
             #                               amount=5, interact=False, randomize=False, sleep_delay=543)
+            session.set_action_delays(enabled=True, like=10, randomize=True,
+                                      random_range_from=50, random_range_to=300)
             session.like_by_locations([
-                '234437663',#Mauna Kea
+                '4573910',  # Hawai`i Community College
+                '214565960',  # Hawaiian Style Café - Hilo
+                '652287',  # Cafe 100
+                '1369906793116040',  # KTA Super Stores
+                '286678182',  # Hilo High School
+                '252552111906405',  # Waiakea High School
+                '220719332',  # Suisan Fish Market
+                '259904138',  # Waikoloa, Hawaii
+                '248933518',  # Waimea Big Island
+                '153047301946370',  # Downtown Hilo
+                '353971',  # Big Island Candies
+                '5123262',  # Waiolena
+                '255285948',  # Keaau, Hawaii
+                '528017',  # University of Hawaiʻi at Hilo
+                '1014734662',  # Manono Street Marketplace
+                '215140125',  # Liliuokalani Park and Gardens
+                '7440188',  # Bayfront Beach Park
+                '234437663',  # Mauna Kea
                 '832944',  # Hilo Farmers Market
                 '217860447',  # Hilo, HI
                 '1400401506921170',  # Kamana Kitchen Indian Cuisine
@@ -60,15 +79,15 @@ if __name__ == '__main__':
                 '213061859',  # Rainbow Falls
                 '753431934796611',  # Hilo Volcanoes National Park and Rainbow Falls Excursion
                 '1683284565326878',  # Boiling Pots, Wailuku River
-            ], amount=5, skip_top_posts=True, randomize=True)
+            ], amount=3, skip_top_posts=True, randomize=True)
             # session.like_by_tags(['waipiovalley', 'maunakea', 'bigislandhawaii'], amount=5, interact=False)
-            session.set_do_like(enabled=True, percentage=60)
-            session.dont_include(['dingteahilo', 'teapressobarbigisland'])
-            session.dont_like(
-                ['#photography', '#kahaluu', '#travel', '#kauai', '#kailuakona', '#maui', 'Maui', 'Mau\'i',
-                 '#Kona', '#Molokai', '#oahu', 'O\'ahu', '#Honolulu',
-                 'www.'])
+            # session.set_do_like(enabled=True, percentage=60)
+            # session.dont_include(['dingteahilo', 'teapressobarbigisland'])
+            # session.dont_like(
+            #     ['#photography', '#kahaluu', '#travel', '#kauai', '#kailuakona', '#maui', 'Maui', 'Mau\'i',
+            #      '#Kona', '#Molokai', '#oahu', 'O\'ahu', '#Honolulu',
+            #      'www.'])
 
-            print_hi('All done. Sleep for 60 minutes...')
-        time.sleep(60 * 60)
+            print_hi('All done. Sleep for 1 hour ...')
+        time.sleep(3600)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
